5105a39c0494f0e2178ab1a0a33991d8703dfcdb
Marek <marekpoliszak@gmail.com>
2023-06-06 19:49:31 +0200

    Added new models

---
+++ src/main/java/com/marekpoliszak/github_repos/model/Commit.java
@@ -0,0 +1,12 @@
+package com.marekpoliszak.github_repos.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Setter
+@Getter
+public class Commit {
+    private String sha;
+}
